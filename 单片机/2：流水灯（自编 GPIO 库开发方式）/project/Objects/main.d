ed725729a8bcdd197520aa74ba669b0dccca3a6d
fanyolo <1803353484@qq.com>
2025-08-05 13:49:43 +0800

    Delete 单片机/2：流水灯（自编 GPIO 库开发方式） directory

--- 单片机/2：流水灯（自编 GPIO 库开发方式）/project/Objects/main.d
+++
@@ -1,4 +0,0 @@
-.\objects\main.o: ..\user\main.c
-.\objects\main.o: ..\user\stm32f10x.h
-.\objects\main.o: ..\user\stm32f10x_gpio.h
-.\objects\main.o: D:\keil5\ARM\ARMCC\Bin\..\include\stdint.h
--- 单片机/2：流水灯（自编 GPIO 库开发方式）/project/Objects/startup_stm32f10x_hd.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_stm32f10x_hd.o: ..\user\startup_stm32f10x_hd.s
